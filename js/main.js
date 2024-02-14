1d9972cbb9ee1e8f03926019b5a846ff5aace2c9
shamandeep03 <shamandeepkaur19@gmail.com>
2024-02-14 23:41:30 +0530

    changes for products

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-(function ($) {
-    "use strict";
-    let products = [
+let products = [
         {
             "id":1,
             "name":"DS-2CE72DF3T-LSE", 
@@ -87,106 +85,100 @@
         }
     ]
     // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
-
-
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+var spinner = function () {
+    setTimeout(function () {
+        var spinnerElement = document.getElementById('spinner');
+        if (spinnerElement) {
+            spinnerElement.classList.remove('show');
         }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
+    }, 1);
+};
+spinner();
+
+// Initiate the wowjs
+new WOW().init();
+
+// Sticky Navbar
+window.addEventListener('scroll', function () {
+    var stickyTopElement = document.querySelector('.sticky-top');
+    if (stickyTopElement) {
+        if (window.scrollY > 300) {
+            stickyTopElement.classList.add('shadow-sm');
+            stickyTopElement.style.top = '0px';
         } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-
-    // Header carousel
-    $(".header-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        items: 1,
-        dots: true,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
-
-    if($('#product-container')){
-        console.log(products)
-        $.each(products, function (index, product) {
-
-            if(index <7  && location.pathname == '/index.html' ){
-                var productHTML = `
-                    <div class="col-md-6 col-lg-3 wow fadeInUp service-main" data-wow-delay="0.1s">
-                        <div class="service-item">
-                            <div class="overflow-hidden" style="display: flex;justify-content: center;">
-                                <img class="img-fluid" src="./img/products/${product.img}.jpeg" alt="${product.name}">
-                            </div>
-                            <div class="p-4 text-center">
-                                <h4 class="mb-3">${product.name}</h4>
-                                <p>${product.heading}</p>
-                                <a class="fw-medium" href="./explore.html?id=${product.id}">Read More<i class="fa fa-arrow-right ms-2"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                `;
-                $('#product-container').append(productHTML)
-            }
-        });
-        if(location.pathname == '/service.html'){
-            $.each(products, function (index, product) {
-                    var productHTML = `
-                        <div class="col-md-6 col-lg-3 wow fadeInUp service-main" data-wow-delay="0.1s">
-                            <div class="service-item">
-                                <div class="overflow-hidden" style="display: flex;justify-content: center;">
-                                    <img class="img-fluid" src="./img/products/${product.img}.jpeg" alt="${product.name}">
-                                </div>
-                                <div class="p-4 text-center">
-                                    <h4 class="mb-3">${product.name}</h4>
-                                    <p>${product.heading}</p>
-                                    <a class="fw-medium" href="./explore.html?id=${product.id}">Read More<i class="fa fa-arrow-right ms-2"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    `;
-                    $('#product-container').append(productHTML)
-            });
+            stickyTopElement.classList.remove('shadow-sm');
+            stickyTopElement.style.top = '-100px';
         }
     }
-})(jQuery);
+});
+
+// Back to top button
+window.addEventListener('scroll', function () {
+    var backToTopElement = document.querySelector('.back-to-top');
+    if (backToTopElement) {
+        if (window.scrollY > 300) {
+            backToTopElement.style.display = 'block';
+        } else {
+            backToTopElement.style.display = 'none';
+        }
+    }
+});
+
+var backToTopButton = document.querySelector('.back-to-top');
+if (backToTopButton) {
+    backToTopButton.addEventListener('click', function () {
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+    });
+}
+
+// Facts counter (no equivalent without using a specific library)
+// If you're using a counter plugin, you'll need to find an alternative in plain JavaScript.
+
+// Header carousel (no equivalent without using a specific library)
+// If you're using an owl carousel, you'll need to find an alternative in plain JavaScript.
+
+// Displaying products based on conditions
+var productContainer = document.getElementById('product-container');
+if (productContainer) {
+    products.forEach(function (product, index) {
+        if (index < 7 && location.pathname === '/index.html') {
+            var productHTML = `
+                <div class="col-md-6 col-lg-3 wow fadeInUp service-main" data-wow-delay="0.1s">
+                    <div class="service-item">
+                        <div class="overflow-hidden" style="display: flex;justify-content: center;">
+                            <img class="img-fluid" src="./img/products/${product.img}.jpeg" alt="${product.name}">
+                        </div>
+                        <div class="p-4 text-center">
+                            <h4 class="mb-3">${product.name}</h4>
+                            <p>${product.heading}</p>
+                            <a class="fw-medium" href="./explore.html?id=${product.id}">Read More<i class="fa fa-arrow-right ms-2"></i></a>
+                        </div>
+                    </div>
+                </div>
+            `;
+            productContainer.insertAdjacentHTML('beforeend', productHTML);
+        }
+    });
+    if (location.pathname === '/service.html') {
+        products.forEach(function (product) {
+            var productHTML = `
+                <div class="col-md-6 col-lg-3 wow fadeInUp service-main" data-wow-delay="0.1s">
+                    <div class="service-item">
+                        <div class="overflow-hidden" style="display: flex;justify-content: center;">
+                            <img class="img-fluid" src="./img/products/${product.img}.jpeg" alt="${product.name}">
+                        </div>
+                        <div class="p-4 text-center">
+                            <h4 class="mb-3">${product.name}</h4>
+                            <p>${product.heading}</p>
+                            <a class="fw-medium" href="./explore.html?id=${product.id}">Read More<i class="fa fa-arrow-right ms-2"></i></a>
+                        </div>
+                    </div>
+                </div>
+            `;
+            productContainer.insertAdjacentHTML('beforeend', productHTML);
+        });
+    }
+}
+
 
