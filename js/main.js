e816ac439ae4338350b310fa1b578a03aad85ea3
shamandeep03 <shamandeepkaur19@gmail.com>
2024-02-14 23:11:12 +0530

    fnial show to client

--- js/main.js
+++ js/main.js
@@ -61,28 +61,6 @@
         ]
     });
 
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            }
-        }
-    });
     if($('#product-container')){
         console.log('hello world')
         $.ajax({
